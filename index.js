0d81ce090421a2c55c2a21738cec3eeda1d2bba6
admin <andrew.n.sadowski@gmail.com>
2018-06-29 11:49:42 -0700

    init commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var webshot = require('node-webshot');
+const fs = require('fs');
+
+const urls = fs.readFileSync('url.txt').toString().split('\n');
+for (i in urls) {
+    console.log(urls[i]);
+}
+
+console.log(urls)
+
+var options = {
+    shotSize: {
+        height: 'all'
+    },
+    renderDelay: 1000
+    // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
+    //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+};
+
+for (i in urls) {
+    webshot(urls[i], `output/${i}.jpeg`, options, function (err) {
+        // screenshot now saved to flickr.jpeg
+    });
+}
+
+webshot('amazon.com', './amazon.png', function (err) {
+    if (err) return console.log(err);
+    console.log('OK');
+});
