37a640b4084fa4572004e6166c4aa8cc93bc0cc7
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-06-29 22:34:53 -0700

    added regexp variable for url parsing

--- index.js
+++ index.js
@@ -1,31 +1,69 @@
 var webshot = require('node-webshot');
 const fs = require('fs');
 
-const urls = fs.readFileSync('url.txt').toString().split('\n');
+const urls = fs
+  .readFileSync('url.txt')
+  .toString()
+  .split('\n');
 for (i in urls) {
-    console.log(urls[i]);
+  console.log(urls[i]);
 }
 
-console.log(urls)
+console.log(urls);
 
 var options = {
-    shotSize: {
-        height: 'all'
-    },
-    renderDelay: 1000
-    // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
-    //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+  shotSize: {
+    height: 'all'
+  },
+  renderDelay: 1000
+  // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
+  //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
 };
 
 for (i in urls) {
-    let urlName = urls[i].replace('https://', '');
-    let finalUrl = urlName.replace('/', '-');
-    webshot(urls[i], `output/${finalUrl}.jpeg`, options, function (err) {
-        if (err) throw err;
-    });
+  let urlName = urls[i].replace('https://', '');
+  let finalUrl = urlName.replace('/', '-');
+  webshot(urls[i], `output/${finalUrl}.jpeg`, options, function(err) {
+    if (err) throw err;
+  });
 }
 
 // webshot('amazon.com', './amazon.png', function (err) {
 //     if (err) return console.log(err);
 //     console.log('OK');
+// });
+// const webshot = require('node-webshot');
+// const fs = require('fs');
+
+// const urls = fs
+//   .readFileSync('url.txt')
+//   .toString()
+//   .split('\n');
+// for (i in urls) {
+//   console.log(urls[i]);
+// }
+
+// console.log(urls);
+
+// var options = {
+//   shotSize: {
+//     height: 'all'
+//   },
+//   renderDelay: 1000
+//   // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
+//   //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+// };
+
+// for (i in urls) {
+//   let regEx = /^(http:\/\/|https:\/\/)/gi;
+//   let urlName = urls[i].replace(regEx, '');
+//   let finalUrl = urlName.replace('/', '-');
+//   console.log(finalUrl);
+//   webshot(urls[i], `output/${i}.jpeg`, options, function(err) {
+//     if (err) throw err;
+//   });
+// }
+// webshot('amazon.com', './amazon.png', function(err) {
+//   if (err) throw err;
+//   console.log('OK');
 // });
