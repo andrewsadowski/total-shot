a5bb8d4f9e9785cb1377538a8d0d04831afac0a1
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-07-08 14:40:42 -0700

    updated regular expressions to replace all forward slashes with hyphens for url name

--- index.js
+++ index.js
@@ -9,21 +9,20 @@ for (i in urls) {
   console.log(urls[i]);
 }
 
-console.log(urls);
-
 var options = {
   shotSize: {
     height: 'all'
   },
-  renderDelay: 1500
+  renderDelay: 2000
   // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
   //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
 };
 
 for (i in urls) {
   let regEx = /^(http:\/\/|https:\/\/)/gi;
+  let finalReg = /\//g;
   let urlName = urls[i].replace(regEx, '');
-  let finalUrl = urlName.replace('/', '-');
+  let finalUrl = urlName.replace(finalReg, '-');
   console.log(finalUrl);
   webshot(urls[i], `output/${finalUrl}.jpeg`, options, function(err) {
     if (err) throw err;
