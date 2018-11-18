1ee9b5c6845feac5d917044c6921ed6518fc3fe9
Andrew Sadowski <andrew.n.sadowski@gmail.com>
2018-11-17 23:58:23 -0800

    moved all logic into execute request function in index.js

--- index.js
+++ index.js
@@ -23,27 +23,31 @@ if (argv.f && argv.o) {
   filePath = './url.txt';
 }
 
-const urls = fs
-  .readFileSync(filePath)
-  .toString()
-  .split('\n');
+const executeRequest = (filePath, outputPath) => {
+  const urls = fs
+    .readFileSync(filePath)
+    .toString()
+    .split('\n');
 
-var options = {
-  shotSize: {
-    height: 'all'
-  },
-  renderDelay: 2000
-  // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
-  //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+  var options = {
+    shotSize: {
+      height: 'all'
+    },
+    renderDelay: 2000
+    // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
+    //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
+  };
+
+  for (i in urls) {
+    let regEx = /^(http:\/\/|https:\/\/)/gi;
+    let finalReg = /\//g;
+    let urlName = urls[i].replace(regEx, '');
+    let finalUrl = urlName.replace(finalReg, '-');
+    console.log(finalUrl);
+    webshot(urls[i], `output/${finalUrl}.jpeg`, options, function(
+      err
+    ) {
+      if (err) throw err;
+    });
+  }
 };
-
-for (i in urls) {
-  let regEx = /^(http:\/\/|https:\/\/)/gi;
-  let finalReg = /\//g;
-  let urlName = urls[i].replace(regEx, '');
-  let finalUrl = urlName.replace(finalReg, '-');
-  console.log(finalUrl);
-  webshot(urls[i], `output/${finalUrl}.jpeg`, options, function(err) {
-    if (err) throw err;
-  });
-}
