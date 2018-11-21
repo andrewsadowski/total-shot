2fc2be86c4450fc704cee87e6a531e0fc43b50c7
admin <andrew.n.sadowski@gmail.com>
2018-11-21 11:45:16 -0800

    updated logging

--- bin/total-shot_global.js
+++ bin/total-shot_global.js
@@ -1,23 +1,19 @@
 #!/usr/bin/env node
 
-const webshot = require('node-webshot');
-const fs = require('fs');
-const path = require('path');
-const chalk = require('chalk');
-const argv = require('yargs')
-  .alias('f', 'filePath')
-  .usage('Usage: add a file path with the -f flag')
+const webshot = require("node-webshot");
+const fs = require("fs");
+const path = require("path");
+const chalk = require("chalk");
+const argv = require("yargs")
+  .alias("f", "filePath")
+  .usage("Usage: add a file path with the -f flag")
   .example('totalShot -f "/absolute/path/to/file.txt"')
-  .alias('o', 'outputPath')
-  .usage(
-    'Usage: Dictate the path you want the screenshots outputted to.'
-  )
+  .alias("o", "outputPath")
+  .usage("Usage: Dictate the path you want the screenshots outputted to.")
   .example('totalShot -o "/absolute/path/to/output"')
-  .help('h').argv;
+  .help("h").argv;
 
-const {
-  checkOrMakeDir
-} = require('../lib/utils');
+const { checkOrMakeDir } = require("../lib/utils");
 
 let filePath;
 let outputPath;
@@ -29,36 +25,36 @@ if (argv.f && argv.o) {
   filePath = argv.f;
   outputPath = checkOrMakeDir();
 } else {
-  filePath = '../url.txt';
-  outputPath = '../output/';
+  filePath = "../url.txt";
+  outputPath = "../output/";
 }
 
 const executeRequest = ((filePath, outputPath) => {
   const urls = fs
     .readFileSync(filePath)
     .toString()
-    .split('\n');
+    .split("\n");
 
   var options = {
     shotSize: {
-      height: 'all'
+      height: "all"
     },
     renderDelay: 2000
     // userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)' +
     //     ' AppleWebKit/531.21.20 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36'
   };
 
+  console.log(chalk.magenta(`Total-Shot running...`));
+
   for (i in urls) {
     let regEx = /^(http:\/\/|https:\/\/)/gi;
     let finalReg = /\//g;
-    let urlName = urls[i].replace(regEx, '');
-    let finalUrl = urlName.replace(finalReg, '-');
+    let urlName = urls[i].replace(regEx, "");
+    let finalUrl = urlName.replace(finalReg, "-");
 
     console.log(
       chalk.yellow.bold(
-        `Writing ${finalUrl} to disc at: ${chalk.green.bold(
-          outputPath
-        )}`
+        `Writing ${finalUrl} to disc at: ${chalk.green.bold(outputPath)}`
       )
     );
     let file = path.join(outputPath, finalUrl);
